d0ec36606b3c86b75112c288847378eab5796ebf
Yerdesha <yerden.abdykarimov@gmail.com>
2021-02-06 22:22:22 +0600

    Initial

---
+++ Assets/Scripts/CatCollect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CatCollect : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Cursor.visible = true;
+
+    }
+
+}
---
+++ Assets/Scripts/MouseConfine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseConfine : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+}
